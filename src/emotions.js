dd5d3bf6d3078024b026cd0d58f29fb611b65769
TheCinemaker <31615891+TheCinemaker@users.noreply.github.com>
2025-11-13 12:02:54 +0100

    Create emotions.js

---
+++ src/emotions.js
@@ -0,0 +1,10 @@
+// src/emotions.js
+export const EMOTIONS = [
+  { id: 'happy', label: '😊', name: 'Boldog' },
+  { id: 'bored', label: '😐', name: 'Unatkozik' },
+  { id: 'stressed', label: '😡', name: 'Stresszes' },
+  { id: 'tired', label: '😴', name: 'Fáradt' },
+  { id: 'motivated', label: '💪', name: 'Motivált' },
+  { id: 'love', label: '❤️', name: 'Szerelmes' },
+  { id: 'hype', label: '🚀', name: 'Hype' }
+];
